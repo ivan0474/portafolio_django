adbe908aef00104f71a3e0e2e647d1edcbff485c
ivanordonez0474 <ivanordonez0474@gmail.com>
2022-06-09 14:53:05 -0400

    first commit

---
+++ portafolio/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+
+def inicio(request):
+    doc_externo = open("static/index.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento)
+    
+def historial(request):
+    doc_externo = open("static/historial.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento)
+
+def Productos(request):
+    doc_externo = open("static/Productos.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento)
+
+def Seguimiento(request):
+    doc_externo = open("static/Seguimiento.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento) 
+    
+def login(request):
+    doc_externo = open("static/login.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento)
+    
+def Register(request):
+    doc_externo = open("static/Register.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento)   
+
+def index(request):
+    doc_externo = open("static/index.css")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento= plt.render(ctx)
+    return HttpResponse(documento)   
